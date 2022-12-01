75023683e8b67ee023134fed61a972c9b41205c2
Ganesh <Ganesh@DESKTOP-N5BO4QE>
2022-12-01 00:39:12 -0800

    commit

---
+++ AnudipAssignment/src/Assignment8/ElectricityBill.java
@@ -0,0 +1,28 @@
+package Assignment8;
+import java.util.*;
+public class ElectricityBill {
+	static int unit;
+	static double Charges = 0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the units : ");
+		unit = s.nextInt();
+		if(unit < 100)
+		{
+			Charges = unit * 1.20;
+			System.out.println(Charges);
+		}
+		else if(100 <= unit && unit<= 300)
+		{
+			Charges = unit * 2.0;
+			System.out.println(Charges);
+		}
+		else
+		{
+			Charges = unit * 3.0;
+			System.out.println(Charges);
+		}
+	}
+}
